a3785190a2f560670e039db6118d74a956826653
Ashish Jha <ashishxyzjha@gmail.com>
2026-06-24 22:36:47 +0530

    file upload config

---
+++ backend/src/utils/cloudinary.ts
@@ -0,0 +1,28 @@
+import { v2 as cloudinary } from "cloudinary";
+import fs from "fs";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export const uploadOnCloudinary = async (file: string) => {
+  try {
+    if (!file) return null;
+
+    // upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(file, {
+      resource_type: "auto",
+    });
+
+    // remove the locally saved file
+    fs.unlinkSync(file);
+
+    return response;
+  } catch (error) {
+    fs.unlinkSync(file);
+
+    return null;
+  }
+};
